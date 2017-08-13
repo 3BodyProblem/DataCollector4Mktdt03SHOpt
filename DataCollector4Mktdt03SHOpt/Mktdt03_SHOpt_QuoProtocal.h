ca079c0c57084ff2f6039009cbae17b30c82666a
BarryPaneer <barry.paneer@qq.com>
2017-08-13 23:20:26 +0800

    updates

--- DataCollector4Mktdt03SHOpt/Mktdt03_SHOpt_QuoProtocal.h
+++ DataCollector4Mktdt03SHOpt/Mktdt03_SHOpt_QuoProtocal.h
@@ -5,34 +5,14 @@
 
 typedef struct
 {
-	char						Code[20];					///< 合约代码
-	char						ContractID[20];				///< 合约代码
-	char						Name[64];					///< 合约名称
-	unsigned char				Kind;						///< 证券类型
-	unsigned char				DerivativeType;				///< 衍生品类型：欧式美式+认购认沽
-	char						UpdateVersion;		//期权合约的版本号(新挂合约是'1')
-	unsigned int				LotSize;					///< 一手等于几张合约
-	char						UnderlyingCode[20];			///< 标的证券代码
-	unsigned int				ContractUnit;				///< 合约单位(经过除权除息调整后的合约单位，一定为整数)
-	unsigned int				XqPrice;					///< 行权价格[*放大倍数]
-	unsigned int				StartDate;					///< 首个交易日(YYYYMMDD)
-	unsigned int				EndDate;					///< 最后交易日(YYYYMMDD)
-	unsigned int				XqDate;						///< 行权日(YYYYMM)
-	unsigned int				DeliveryDate;				///< 交割日(YYYYMMDD)
-	unsigned int				ExpireDate;					///< 到期日(YYYYMMDD)
-	char						StatusFlag[8];		//期权合约状态(8个字符,详细定义见文档)
-} tagSHOptReferenceData_LF108;
-
-
-typedef struct
-{
-	char						Key[32];					///< 索引键值
+	char						Key[20];					///< 索引键值
 	unsigned int				MarketID;					///< 市场编号
+	unsigned int				MarketDate;					///< 市场日期
 	unsigned int				KindCount;					///< 类别数量
 	unsigned int				WareCount;					///< 商品数量
 	unsigned int				PeriodsCount;				///< 交易时段信息列表长度
 	unsigned int				MarketPeriods[8][2];		///< 交易时段描述信息列表
-} tagSHOptMarketInfo_LF109;
+} tagSHOptMarketInfo_LF157;
 
 
 typedef struct
@@ -42,7 +22,36 @@ typedef struct
 	unsigned int				PriceRate;					///< 价格放大倍数[10的多少次方]
 	unsigned int				LotSize;					///< “手”比率
 	unsigned int				WareCount;					///< 该分类的商品数量
-} tagSHOptKindDetail_LF110;
+} tagSHOptKindDetail_LF158;
+
+
+typedef struct
+{
+	char						Key[20];					///< 索引键值
+	unsigned int				MarketTime;					///< 市场时间
+	char						StatusFlag[8];				///< 市场状态(8个字符,详细定义见文档)
+} tagSHOptMarketStatus_HF159;
+
+
+typedef struct
+{
+	char						Code[20];					///< 合约代码
+	char						ContractID[20];				///< 合约代码
+	char						Name[64];					///< 合约名称
+	unsigned short				Kind;						///< 证券类型
+	unsigned char				DerivativeType;				///< 衍生品类型：欧式美式+认购认沽
+	char						UpdateVersion;				///< 期权合约的版本号(新挂合约是'1')
+	unsigned int				LotSize;					///< 一手等于几张合约
+	char						UnderlyingCode[20];			///< 标的证券代码
+	unsigned int				ContractUnit;				///< 合约单位(经过除权除息调整后的合约单位，一定为整数)
+	unsigned int				XqPrice;					///< 行权价格[*放大倍数]
+	unsigned int				StartDate;					///< 首个交易日(YYYYMMDD)
+	unsigned int				EndDate;					///< 最后交易日(YYYYMMDD)
+	unsigned int				XqDate;						///< 行权日(YYYYMM)
+	unsigned int				DeliveryDate;				///< 交割日(YYYYMMDD)
+	unsigned int				ExpireDate;					///< 到期日(YYYYMMDD)
+	char						StatusFlag[8];				///< 期权合约状态(8个字符,详细定义见文档)
+} tagSHOptReferenceData_LF160;
 
 
 typedef struct
@@ -56,7 +65,10 @@ typedef struct
 	unsigned int				SettlePrice;				///< 今结价[*放大倍数]
 	unsigned int				PreSettlePrice;				///< 合约昨结[*放大倍数]
 	unsigned __int64			PreOpenInterest;			///< 昨日持仓量(张)
-} tagSHOptSnapData_LF112;
+	unsigned int				AuctionPrice;				///< 波动性中断参考价[*放大倍数]
+	unsigned __int64			AuctionQty;					///< 波动性中断集合竞价虚拟量
+	char						TradingPhaseCode[4];		///< 交易状态
+} tagSHOptSnapData_LF161;
 
 
 typedef struct
@@ -68,32 +80,22 @@ typedef struct
 	double						Amount;						///< 总成交金额[元]
 	unsigned __int64			Volume;						///< 总成交量[股/张]
 	unsigned __int64			Position;					///< 持仓量
-} tagSHOptSnapData_HF113;
+} tagSHOptSnapData_HF162;
 
 
 typedef struct
 {
 	unsigned int				Price;						///< 委托价格[* 放大倍数]
 	unsigned __int64			Volume;						///< 委托量[股]
-} tagBuySellItem;
+} tagSHOptBuySellItem;
 
 
 typedef struct
 {
 	char						Code[20];					///< 合约代码
-	tagBuySellItem				Buy[5];						///< 买五档
-	tagBuySellItem				Sell[5];					///< 卖五档
-} tagSHOptSnapBuySell_HF114;
-
-
-typedef struct
-{
-	char						Key[32];					///< 索引键值
-	unsigned int				MarketDate;					///< 市场日期
-	unsigned int				MarketTime;					///< 市场时间
-	unsigned char				MarketStatus;				///< 市场状态[0初始化 1行情中]
-} tagSHL1MarketStatus_HF105;
-
+	tagSHOptBuySellItem			Buy[5];						///< 买五档
+	tagSHOptBuySellItem			Sell[5];					///< 卖五档
+} tagSHOptSnapBuySell_HF163;
 
 
 #pragma pack()
